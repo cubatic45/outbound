a7a5c727a48d34db793012d5092b884beacec56b
kix <olices@9up.in>
2026-02-28 14:00:20 +0800

    chore: remove unused test interface for FakeNetPacketConn

--- test_interface.go
+++
@@ -1,8 +0,0 @@
-package shadowsocks_2022
-
-import (
-    "github.com/daeuniverse/outbound/netproxy"
-)
-
-// Verify that FakeNetPacketConn implements netproxy.PacketConn
-var _ netproxy.PacketConn = &FakeNetPacketConn{}
